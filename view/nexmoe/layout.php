f22f0bc32a3d3656d5adf02f7c450a3981903995
July's <37677092+july929@users.noreply.github.com>
2020-05-08 23:52:10 +0800

    Update layout.php

--- view/nexmoe/layout.php
+++ view/nexmoe/layout.php
@@ -9,8 +9,37 @@
 	<script src="//cdn.bootcss.com/jquery/1.12.3/jquery.min.js"></script>	
 	<script src="//cdn.staticfile.org/layer/2.3/layer.js"></script>
 	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css">
-	<script src="//cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css">
+	<link rel="shortcut icon" href="https://yangwenqing.com/Logo.png">
+
 	 <style>
+        .mdui-theme-primary-indigo .mdui-color-theme {
+            color: #fff !important;
+            background-color: #1199c4 !important;
+        }
+        .mdui-grid-tile {
+            position: relative;
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+            margin-bottom: 10px;
+            overflow: hidden;
+            border-radius: 50%;
+            width: 120px;
+            margin-left: 40px;
+            margin-top: 10px;
+        }
+       .mdui-drawer {
+            width: 200px;
+            max-width: none;
+        }
+       .mdui-color-indigo-50 {
+            color: rgba(0, 0, 0, .87) !important;
+            background-color: #dde6e9 !important;
+        }
+        [class*="mdui-color-"]
+       .mdui-list-item {
+            color: #777d7f;
+        }
 		.mdui-appbar .mdui-toolbar			
 			height:56px;
 			font-size: 10px;
@@ -53,14 +82,14 @@
 		#instantclick-bar {
         		background: white;
         	}
-		.mdui-img-fluid, .mdui-video-fluid {
+		.mdui-video-fluid {
             height: -webkit-fill-available;
         }
 		.dplayer-video-wrap .dplayer-video {
 			height: -webkit-fill-available !important;
 		}
         .gslide iframe, .gslide video {
-           height: -webkit-fill-available;
+            height: -webkit-fill-available;
         }
 		@media screen and (max-width:950px)	{		
 			.mdui-list-item .mdui-text-right{
@@ -73,7 +102,7 @@
 			.mdui-toolbar>*{				
 				display: none;
 			}
-			.mdui-toolbar>a:last-child,.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>i:first-child{
+			.mdui-toolbar>a:last-child,.mdui-toolbar>a:nth-last-of-type(2),.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>i:first-child,.mdui-toolbar-spacer{
 				display: block;
 			}
 		}
@@ -86,6 +115,10 @@
 		#dl-header{margin:0}
 		#dl-section{padding-top:10px}
 		#dl-latest{position:relative;top:50%;transform:translateY(-50%)}
+       .oneindex{
+            text-align: center;
+            color: #777d7f;
+       }
 	</style>
 </head>
 <body class=" mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink">
@@ -97,19 +130,16 @@
 			<i class="mdui-icon material-icons mdui-icon-dark" style="margin:0;">chevron_right</i>
 			<a href="<?php e($l);?>"><?php e($n);?></a>
 		<?php endforeach;?>
+		<div class="mdui-toolbar-spacer"></div>
+		<a href="javascript:thumb();" id="thumb" class="mdui-btn mdui-btn-icon" mdui-tooltip="{content: '切换显示'}"><i class="mdui-icon material-icons">format_list_bulleted</i></a>
 	</div>	
 	</header>
 
 <div class="mdui-drawer mdui-drawer-close mdui-color-indigo-50" id="main-drawer">
 	<div class="mdui-grid-tile">
-		<a href="javascript:;"><img src="//p.ssl.qhimg.com/t01ad8dba0b68972b26.jpg"/></a>
-		<div class="mdui-grid-tile-actions mdui-grid-tile-actions-gradient">
-			<div class="mdui-grid-tile-text">
-				<div class="mdui-grid-tile-title"><?php e($title.' - '.config('site_name'));?></div>
-				<div class="mdui-grid-tile-subtitle">Onedrive</div>
-    			</div>
-   		 </div>
-	</div>   
+		<a href="javascript:;"><img src="https://yangwenqing.com/Logo.png"/></a>
+	</div> 
+ <div class="oneindex">Oneindex</div> 
 	<div class="mdui-list" mdui-collapse="{accordion: true}">
 		<a href="/" class="mdui-list-item mdui-ripple">
 			<i class="mdui-list-item-icon mdui-icon material-icons">home</i>
